2677b4ce76c8a44376ce53e012a4ae788ac9f5c3
AIINSTARAJ <tomiwakuteyi@gmail.com>
2025-01-10 11:56:12 +0100

    Run Simulation

--- DHT_.cpp
+++ DHT_.cpp
@@ -1,13 +1,7 @@
 #include "DHT.h"
 #define DHTTYPE DHT22 
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
 
-const int DHTPin = 5;     // what digital pin we're connected to
+const int DHTPin = 5;
 
 DHT dht(DHTPin, DHTTYPE);
 
