c458fbb9b2e74d4180217609f9b3279e439a295e
AIINSTARAJ <tomiwakuteyi@gmail.com>
2025-01-10 11:54:04 +0100

    Removed Comment

--- DHT_.cpp
+++ DHT_.cpp
@@ -1,10 +1,5 @@
 #include "DHT.h"
-
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
+#define DHTTYPE DHT22 
 // Connect pin 1 (on the left) of the sensor to +5V
 // NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
 // to 3.3V instead of 5V!
