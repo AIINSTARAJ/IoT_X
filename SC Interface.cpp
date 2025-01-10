16a32900b5ea4eacbcd1ad0a5887c5b64eba07be
AIINSTARAJ <tomiwakuteyi@gmail.com>
2025-01-10 10:49:13 +0100

    SpeedX

--- SC Interface.cpp
+++ SC Interface.cpp
@@ -20,7 +20,7 @@ void loop(){
         digitalWrite(3, LOW); 
         digitalWrite(4, LOW);
         digitalWrite(5, HIGH);
-        delay(250); 
+        delay(500); 
         digitalWrite(4, HIGH); 
         digitalWrite(5, LOW);
         delay(250); 
